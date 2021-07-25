03c015f07e43c22debfa9559b9f4886afddb4477
anilburakbilsel <abbilsel@gmail.com>
2021-07-24 19:06:36 -0700

    code changes

--- LambdaEFS/app.js
+++ LambdaEFS/app.js
@@ -7,6 +7,37 @@ exports.snsPayloadLoggerHandler = async (event, context) => {
     console.log(event);
 }
 
+const os = require('os')
+
+const inputFile = process.env.INPUT_FILE
+const efsPath = process.env.EFS_PATH
+
+const { exec } = require('child_process')
+
+const execPromise = async (command) => {
+	console.log(command)
+	return new Promise((resolve, reject) => {
+		const ls = exec(command, function (error, stdout, stderr) {
+		  if (error) {
+		    console.log('Error: ', error)
+		    reject(error)
+		  }
+		  console.log('stdout: ', stdout);
+		  console.log('stderr: ' ,stderr);
+		})
+		
+		ls.on('exit', function (code) {
+		  console.log('Finished: ', code);
+		  resolve()
+		})
+	})
+}
+
+// The Lambda handler
+exports.handler = async function (eventObject, context) {
+	await execPromise(`/opt/bin/ffmpeg -loglevel error -i ${efsPath}/${inputFile} -s 240x135 -vf fps=1 ${efsPath}/%d.jpg`)
+}
+
 // import json
 // from pathlib import Path
 
